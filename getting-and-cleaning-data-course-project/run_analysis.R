92024062b1e3351e9234211d4ffc6205292052a1
Rahul0310 <rahul712@gmail.com>
2017-01-18 09:22:37 +0530

    Update run_analysis.R

--- getting-and-cleaning-data-course-project/run_analysis.R
+++ getting-and-cleaning-data-course-project/run_analysis.R
@@ -1,19 +1,24 @@
+##Extracted Dataset from URL to Local
 if(!file.exists("/Users/rshaury/Desktop/R/R_Programming/data")){dir.create("/Users/rshaury/Desktop/R/R_Programming/data")}
 fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 download.file(fileUrl,destfile="/Users/rshaury/Desktop/R/R_Programming/data/Dataset.zip",method="curl")
 
+##Unzip File
 unzip(zipfile="./data/Dataset.zip",exdir="./data")
 
 path_rf <- file.path("/Users/rshaury/Desktop/R/R_Programming/data" , "UCI HAR Dataset")
 files<-list.files(path_rf, recursive=TRUE)
 files
 
+##Read the Activity files
 dataActivityTest  <- read.table(file.path(path_rf, "test" , "Y_test.txt" ),header = FALSE)
 dataActivityTrain <- read.table(file.path(path_rf, "train", "Y_train.txt"),header = FALSE)
 
+##Read the Subject files
 dataSubjectTrain <- read.table(file.path(path_rf, "train", "subject_train.txt"),header = FALSE)
 dataSubjectTest  <- read.table(file.path(path_rf, "test" , "subject_test.txt"),header = FALSE)
 
+##Read Fearures files
 dataFeaturesTest  <- read.table(file.path(path_rf, "test" , "X_test.txt" ),header = FALSE)
 dataFeaturesTrain <- read.table(file.path(path_rf, "train", "X_train.txt"),header = FALSE)
 
@@ -24,21 +29,25 @@ str(dataSubjectTest)
 str(dataFeaturesTest)
 str(dataFeaturesTrain)
 
-
+##Concatenate the data tables
 dataSubject <- rbind(dataSubjectTrain, dataSubjectTest)
 dataActivity<- rbind(dataActivityTrain, dataActivityTest)
 dataFeatures<- rbind(dataFeaturesTrain, dataFeaturesTest)
 
+##Name Variables
 names(dataSubject)<-c("subject")
 names(dataActivity)<- c("activity")
 dataFeaturesNames <- read.table(file.path(path_rf, "features.txt"),head=FALSE)
 names(dataFeatures)<- dataFeaturesNames$V2
 
+##Create Dataframe Data
 dataCombine <- cbind(dataSubject, dataActivity)
 Data <- cbind(dataFeatures, dataCombine)
 
+##mean and standard deviation
 subdataFeaturesNames<-dataFeaturesNames$V2[grep("mean\\(\\)|std\\(\\)", dataFeaturesNames$V2)]
 
+##Subset the data frame Data by seleted names of Features
 selectedNames<-c(as.character(subdataFeaturesNames), "subject", "activity" )
 Data<-subset(Data,select=selectedNames)
 
@@ -49,7 +58,12 @@ activityLabels <- read.table(file.path(path_rf, "activity_labels.txt"),header = 
 
 head(Data$activity,30)
 
-
+##prefix t is replaced by time
+##Acc is replaced by Accelerometer
+##Gyro is replaced by Gyroscope
+##prefix f is replaced by frequency
+##Mag is replaced by Magnitude
+##BodyBody is replaced by Body
 names(Data)<-gsub("^t", "time", names(Data))
 names(Data)<-gsub("^f", "frequency", names(Data))
 names(Data)<-gsub("Acc", "Accelerometer", names(Data))
